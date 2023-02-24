197f0baf690dd547bbd35f8a20b353908adba79a
Michael Coetzee <mcoetzee@yellowtail.nl>
2023-02-24 11:08:35 +0200

    update requests and dictionaries

--- get_bms_token_updated.py
+++ get_bms_token_updated.py
@@ -34,8 +34,9 @@ resp_dict = r.json()
 token = resp_dict['result']['accessToken']
 
 sort_params = {
-  "orderby": "id",
-  "top": "15"
+  "order_by": "id",
+  "top": "3",
+  "sort_by": "desc"
 }
 
 get_headers = {
@@ -45,26 +46,36 @@ get_headers = {
 
 g = requests.get("https://api.bmsemea.kaseya.com/v2/servicedesk/tickets", params=sort_params, headers=get_headers)
 get_dict = g.json()
+
 account_names = get_dict['result']
 
-data = []
+ticket_ids = []
+for x in account_names:
+    ticket_ids.append(x['id'])
+
+num = os.getenv('top_tickets')
+
+print(num)
+
+ticket_id_top = ticket_ids[:int(num)]
+
+tickets = []
+for z in ticket_id_top:
+    match = next(d for d in account_names if d['id'] == z)
+    tickets.append(match)
+
 
 post_headers = {
   "accept": "application/json",
   "Content-Type": "application/json"
 }
 
-for i in account_names:
+for i in tickets:
+    data = []
     dictionary = {'accountName': i['accountName'], 'dueDate': i['dueDate'], 'lastActivityUpdate': i['lastActivityUpdate'], 'openDate': i['openDate'], 
     'queueName': i['queueName'], 'ticketNumber': i['ticketNumber'], 'id': i['id'], 'title': i['title'], 'priorityName': i['priorityName'], 
     'statusName': i['statusName'], 'slaName': i['slaName'], 'assigneeName': i['assigneeName'], 'hasMetSLA': i['hasMetSLA'], 'slaStatusEnum': i['slaStatusEnum'], 
     'slaStatusEventId': i['slaStatusEventId']}
-
-    # try:
-    #     requests.post(os.getenv('powerBi_API'), json=dictionary, headers=post_headers)
-    # except Exception as e:
-    #     print(e)
-    #     continue
 
     ticket_num = i['id']
     sort_params = {
@@ -74,21 +85,9 @@ for i in account_names:
     get_dict = s.json()
     sla_info = get_dict['result']
 
-    if sla_info:
-            dictionary2 = {'respondGoal': sla_info['respondGoal'], 'respondActual': sla_info['respondActual'], 'respondRemaining': sla_info['respondRemaining'], 'resolveGoal': sla_info['resolveGoal'], 
-            'resolveActual': sla_info['resolveActual'], 'resolveRemaining': sla_info['resolveRemaining'], 'waitingActual': sla_info['waitingActual'], 'reopenActual': sla_info['reopenActual'], 
-            'timeTotalGoal': sla_info['timeTotalGoal'], 'timeTotalActual': sla_info['timeTotalActual'], 'timeTotalRemaining': sla_info['timeTotalRemaining'], 'ticketPriorityColor': sla_info['ticketPriorityColor']}
-
-dict_3 = dictionary.copy()
-dict_3.update(dictionary2)
-print(dict_3)
-
-# dictionary3 = Merge(dictionary, dictionary2)
-# data.append(dict(dictionary, **dictionary2))
-
-for item in dict_3:
-    try:
-        requests.post(os.getenv('powerBi_API'), json=dict_3, headers=post_headers)
-    except Exception as e:
-        print(e)
-        continue
+    dictionary2 = {'respondGoal': sla_info['respondGoal'], 'respondActual': sla_info['respondActual'], 'respondRemaining': sla_info['respondRemaining'], 'resolveGoal': sla_info['resolveGoal'], 
+    'resolveActual': sla_info['resolveActual'], 'resolveRemaining': sla_info['resolveRemaining'], 'waitingActual': sla_info['waitingActual'], 'reopenActual': sla_info['reopenActual'], 
+    'timeTotalGoal': sla_info['timeTotalGoal'], 'timeTotalActual': sla_info['timeTotalActual'], 'timeTotalRemaining': sla_info['timeTotalRemaining'], 'ticketPriorityColor': sla_info['ticketPriorityColor']}
+    dictionary.update(dictionary2)
+    data.append(dictionary)
+    requests.post(os.getenv('powerBi_API'), json=data, headers=post_headers)
